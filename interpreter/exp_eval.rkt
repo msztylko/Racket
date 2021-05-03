116df8b4f89b4a12df413216839bbc2fc52ee33f
Marcin Sztylko <marcin.sztylko@gmail.com>
2021-05-03 13:59:58 +0200

    add exp_eval

---
+++ interpreter/exp_eval.rkt
@@ -0,0 +1,76 @@
+#lang racket
+
+(define my-list (list 1 "adf" 2 "adsf" 3 "asdffg" 5 "qer" 4 "dafew"))
+
+(define (funny-sum xs)
+  (cond [(null? xs) 0]
+        [(number? (car xs)) (+ (car xs) (funny-sum (cdr xs)))]
+        [(string? (car xs)) (+ (string-length (car xs)) (funny-sum (cdr xs)))]))
+
+; new language implementation
+(struct const (int) #:transparent)
+(struct negate (e) #:transparent)
+(struct add (e1 e2) #:transparent)
+(struct multiply (e1 e2) #:transparent)
+(struct bool (b) #:transparent)
+(struct if-then-else (e1 e2 e3) #:transparent)
+(struct eq-num (e1 e2) #:transparent)
+
+; valid AST, should return result
+(define test1 (multiply (negate (add (const 2)
+                                     (const 2)))
+                        (const 7)))
+; valid AST, wrong usage, raise error
+(define test2 (multiply (negate (add (const 2)
+                                     (const 2)))
+                        (if-then-else (bool #f)
+                                      (const 7)
+                                      (bool #t))))
+; invalif AST
+(define non-test (multiply (negate (add (const #t)
+                                        (const 2)))
+                           (const 7)))
+ [(multiply? e)
+         (let ([v1 (eval-exp (multiply-e1 e))]
+               [v2 (eval-exp (multiply-e2 e))])
+           (if (and (const? v1) (const? v2))
+               (const (* (const-int v1) (const-int v2)))
+               (error "multiply applied to non-number")))]
+; eval-exp i.e. new language interpreter
+
+(define (eval-exp e)
+  (cond [(const? e)
+         e]
+        [(negate? e)
+         (let ([v (eval-exp (negate-e e))])
+           (if (const? v)
+               (const (- (const-int v)))
+               (error "negate applied to non-number")))]
+        [(add? e)
+         (let ([v1 (eval-exp (add-e1 e))]
+               [v2 (eval-exp (add-e2 e))])
+           (if (and (const? v1) (const? v2))
+               (const (+ (const-int v1) (const-int v2)))
+               (error "add applied to non-number")))]
+        [(multiply? e)
+         (let ([v1 (eval-exp (multiply-e1 e))]
+               [v2 (eval-exp (multiply-e2 e))])
+           (if (and (const? v1) (const? v2))
+               (const (* (const-int v1) (const-int v2)))
+               (error "multiply applied to non-number")))]
+        [(bool? e)
+         e]
+        [(eq-num? e)
+         (let ([v1 (eval-exp (eq-num-e1 e))]
+               [v2 (eval-exp (eq-num-e2 e))])
+           (if (and (const? v1) (const? v2))
+               (bool (= (const-int v1) (const-int v2)))
+               (error "eq-num applied to non-number")))]
+        [(if-then-else? e)
+         (let ([v-test (eval-exp (if-then-else-e1 e))])
+           (if (bool? v-test)
+               (if (bool-b v-test)
+                   (eval-exp (if-then-else-e2 e))
+                   (eval-exp (if-then-else-e3 e)))
+               (error "if-then-else applied to non-bool")))]
+        [#t (error "eval-exp expected an exp")]))
